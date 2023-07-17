e6f055b19c48799b47f6c597954869a6d3ebfbbe
Dashaun <dashaunriggins@gmail.com>
2023-07-17 01:10:53 -0500

    second commit

---
+++ alembic/versions/792e26c2cdcd_add_user_table.py
@@ -0,0 +1,24 @@
+"""add user table
+
+Revision ID: 792e26c2cdcd
+Revises: 
+Create Date: 2023-07-16 18:14:30.961466
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '792e26c2cdcd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
