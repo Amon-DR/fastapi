58d633b85656b2b869cd6e34c01fd70e32e17483
Dashaun <dashaunriggins@gmail.com>
2023-07-18 22:37:09 -0500

    middleware update

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_origins = origins,
     allow_credentials = True,
     allow_methods=["*"],
-    allow_header=["*"]
+    allow_headers=["*"]
 )
 # def find_post(id):
 #     for p in my_posts:
